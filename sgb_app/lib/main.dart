6094a7ece4fa315e549c94c2fb3306f51830e2b9
Ronald Vega <ronald.vega48@gmail.com>
2019-02-25 11:48:39 -0600

    Diseño del UI para pagina de inicio
    Primera propuesta para el UI la aplicacion

--- sgb_app/lib/main.dart
+++ sgb_app/lib/main.dart
@@ -3,21 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,15 +20,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +27,175 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final scaffoldKey =new GlobalKey<ScaffoldState>();
+  final formKey =new GlobalKey<FormState>();
+  bool _isLoading =false, _userFound = false;
+  String _email, _password;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+//----------------------------------------------------------Funciones que cargan el Loading----------------------------------------------------------
+  _showLoading() {
+		setState(() {
+		  _isLoading = true;
+		});
+    CircularProgressIndicator();
+	}
+
+  _hideLoading() {
+		setState(() {
+		  _isLoading = false;
+		});
   }
 
+
+
+
+
+
+
+
+
+//----------------------------------------------------------Metodo para hacer Login----------------------------------------------------------
+  
+  void _login(){
+    final form = formKey.currentState;
+    if(form.validate()){
+      form.save();
+      _showLoading();
+      //performLogin();
+    }
+  }
+
+//----------------------------------------------------------Creando botones e Interfaz grafica----------------------------------------------------------
+  TextStyle style =TextStyle(fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.black);
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  Widget build(BuildContext context){
+//Email
+    final emailField = TextFormField(
+      validator: (val) => !val.contains('@') ? 'Invalid Email' :null,
+      onSaved: (val) => _email = val,
+      style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.black),
+      decoration: InputDecoration(
+        labelText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0)
+        )
       ),
+    );
+//Password
+    final passwordField =TextFormField(
+      validator: (val) => !(val.length > 5) ? 'Invalid Password' :null,
+      onSaved: (val) => _password = val,
+      obscureText: true,
+      style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.black),
+      decoration: InputDecoration(
+        labelText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0 , 15.0 , 20.0, 15.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0)
+        )
+      ),
+    );
+//Login
+    final loginButton = Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(14.0),
+      color: Color.fromRGBO(23, 17, 85, 1),
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+        onPressed: _login,
+        child: Text("Entrar",
+          textAlign: TextAlign.center,
+          style: style.copyWith(
+            color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
+      );
+//SingUp
+      final signUpButton = Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(14.0),
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(16.0, 15.0, 16.0, 15.0),
+        onPressed: (){},
+        child: Text("Registrarse",
+          textAlign: TextAlign.center,
+          style: style.copyWith(
+            color: Colors.black, fontWeight: FontWeight.bold)),
+        ),
+      );
+//Olvido su usuario
+      final forgotUser = FlatButton(
+        onPressed: (){},
+        child: Text("¿Olvidó su usuario?",
+        textScaleFactor: 0.9,
+          textAlign: TextAlign.center,
+          style: style.copyWith(
+            color: Colors.black54, fontWeight: FontWeight.bold)),
+        );
+
+//Olvido su contraseña
+      final forgotPassword = FlatButton(
+        onPressed: (){},
+        child: Text("¿Olvidó su contraseña?",
+        textScaleFactor: 0.9,
+          textAlign: TextAlign.center,
+          style: style.copyWith(
+            color: Colors.black54, fontWeight: FontWeight.bold)),
+        );
+
+//Creando la interfaz grafica
+    return Scaffold(
+      backgroundColor: Colors.white,
+      key: scaffoldKey,
+      resizeToAvoidBottomPadding: true,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: new Form(
+          key:formKey,
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child:SingleChildScrollView(
+              reverse: true,
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 150.0,
+                  child: Image.asset("assets/logo_sgb.png",
+                  fit:BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 45.0),
+                //Email
+                emailField,
+                SizedBox(height: 25.0),
+                //Password
+                passwordField,
+                SizedBox(height: 35.0),
+                //Botones 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    //Login
+                    loginButton,
+                    SizedBox(height: 25.0),
+                    //Registrarse
+                    signUpButton
+                    ],
+                  ),
+                SizedBox(height: 15.0),
+                forgotUser,
+                SizedBox(height: 1.0),
+                forgotPassword,
+                ], 
+              ),
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
