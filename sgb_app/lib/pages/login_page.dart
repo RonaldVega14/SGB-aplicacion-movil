ae06244312f009841b0e41bc928d7dd5ca41f28c
Ronald Vega <ronald.vega48@gmail.com>
2019-03-01 13:59:37 -0600

    New Logo and UI Changes

--- sgb_app/lib/pages/login_page.dart
+++ sgb_app/lib/pages/login_page.dart
@@ -89,66 +89,54 @@ class LoginScreen extends StatefulWidget {
         ),
       );
 //SingUp
-      final signUpButton = Material(
-      elevation: 3.0,
-      borderRadius: BorderRadius.circular(16.0),
-      child: MaterialButton(
-        padding: EdgeInsets.fromLTRB(12.0, 1.0, 12.0, 1.0),
+    final signUpButton = Material(
+    elevation: 3.0,
+    borderRadius: BorderRadius.circular(16.0),
+    child: MaterialButton(
+      padding: EdgeInsets.fromLTRB(12.0, 1.0, 12.0, 1.0),
+      onPressed: (){},
+      child: Text("Registrarse",
+        textAlign: TextAlign.center,
+        style: style.copyWith(
+          color: Colors.black, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+//Construye el boton de olvido<info>?
+    Widget _forgotInfo(String info){
+      return FlatButton(
         onPressed: (){},
-        child: Text("Registrarse",
-          textAlign: TextAlign.center,
-          style: style.copyWith(
-            color: Colors.black, fontWeight: FontWeight.bold)),
-        ),
-      );
-//Olvido su usuario
-      final forgotUser = FlatButton(
-        onPressed: (){},
-        child: Text("¿Olvidó su usuario?",
-        textScaleFactor: 1,
+        child: Text(info,
+        textScaleFactor: 0.8,
           textAlign: TextAlign.center,
           style: style.copyWith(
             color: Colors.black54, fontWeight: FontWeight.bold)),
         );
+    }
 
-//Olvido su contraseña
-      final forgotPassword = FlatButton(
-        onPressed: (){},
-        child: Text("¿Olvidó su contraseña?",
-        textScaleFactor: 1,
-          textAlign: TextAlign.center,
-          style: style.copyWith(
-            color: Colors.black54, fontWeight: FontWeight.bold)),
-        );
-
-//Creando la interfaz grafica
-    return Scaffold(
-      backgroundColor: Colors.white,
-      key: scaffoldKey,
-      resizeToAvoidBottomPadding: true,
-      body: Center(
-        child: new Form(
-          key:formKey,
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            alignment: Alignment(0, 0),
-            color: Colors.white,
-            child: Padding(
-            padding: const EdgeInsets.fromLTRB(36.0, 25.0, 36.0, 15.0),
-            child:SingleChildScrollView(
-              reverse: true,
-              child: Column(
+//Construye el contenido
+  Widget _buildContent(){
+    return new Form(
+      key:formKey,
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(36.0, 0.0, 36.0, 20.0),
+          child:SingleChildScrollView(
+            reverse: false,
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SizedBox(height: 25.0),
+                SizedBox(height: MediaQuery.of(context).size.height*0.10),
                 SizedBox(
-                  height: 80.0,
+                  height: MediaQuery.of(context).size.height*0.13,
                   child: Image.asset("assets/logo_sgb.png",
                   fit:BoxFit.contain,
                   ),
                 ),
-                SizedBox(height: 45.0),
+                SizedBox(height: MediaQuery.of(context).size.height*0.08),
                 //Email
                 emailField,
                 SizedBox(height: 25.0),
@@ -163,24 +151,35 @@ class LoginScreen extends StatefulWidget {
                     loginButton,
                     //Registrarse
                     signUpButton,
+                  ],
+                ),
+                Divider(height: 65.0, color: Colors.black, indent: 0.0,),
+                Container(
+                  margin: const EdgeInsets.only(top:10.0),
+                  height: MediaQuery.of(context).size.height*0.12,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(child: _forgotInfo("¿Olvidó su usuario?"), flex: 1, fit: FlexFit.tight),
+                      Flexible(child: _forgotInfo("¿Olvidó su contraseña?"), flex: 1, fit: FlexFit.tight),
                     ],
                   ),
-                Divider(height: 65.0, color: Colors.black),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    forgotUser,
-                    forgotPassword,
-                ],
                 ),
-                ], 
-              ),
-              ),
+              ], 
             ),
           ),
         ),
       ),
     );
   }
-      
+//Creando la interfaz grafica
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: false,
+      key: scaffoldKey,
+      body: Center(
+        child: _buildContent()
+      ),
+    );
+  }
 }
